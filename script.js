7a9ef262236ea56f47b2ee005e8b194c8473696f
Biademery <biademery@gmail.com>
2022-09-27 13:53:12 -0300

    add javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+const correctAnswers = [
+  "question1d",
+  "question2d",
+  "question3b",
+  "question4d",
+  "question5e",
+  "question6c",
+  "question7c",
+  "question8a",
+  "question9c",
+  "question10d",
+];
+
+console.log();
+
+function checkAnswers(event) {
+  event.preventDefault();
+
+  let i;
+  for (i = 0; i < 10; i++) {
+    const isCorrect = document.getElementById(correctAnswers[i]).checked;
+    const currentParagraph = document.getElementsByClassName("result")[i];
+
+    if (isCorrect) {
+      currentParagraph.textContent = "Correct!";
+    } else {
+      currentParagraph.textContent = "Wrong!";
+    }
+  }
+}
