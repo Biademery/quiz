81a993c07d4f33e2e0306d4aaec052273baba776
Bia D'Emery <biademery@gmail.com>
2022-12-07 20:23:21 -0300

    change the background color and some adjustments

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const correctAnswers = [
-  "question1d",
-  "question2d",
-  "question3b",
-  "question4d",
-  "question5e",
-  "question6c",
-  "question7c",
-  "question8a",
-  "question9c",
-  "question10d",
+  'question1d',
+  'question2d',
+  'question3b',
+  'question4d',
+  'question5e',
+  'question6c',
+  'question7c',
+  'question8a',
+  'question9c',
+  'question10d',
 ];
 
 console.log();
@@ -17,14 +17,15 @@ function checkAnswers(event) {
   event.preventDefault();
 
   let i;
+
   for (i = 0; i < 10; i++) {
     const isCorrect = document.getElementById(correctAnswers[i]).checked;
-    const currentParagraph = document.getElementsByClassName("result")[i];
+    const currentParagraph = document.getElementsByClassName('result')[i];
 
     if (isCorrect) {
-      currentParagraph.textContent = "Correct!";
+      currentParagraph.textContent = 'Correct!';
     } else {
-      currentParagraph.textContent = "Wrong!";
+      currentParagraph.textContent = 'Wrong!';
     }
   }
 }
