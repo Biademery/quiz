fe1c96cd20cbc0760daa2155b766de9903c32cff
Bia D'Emery <biademery@gmail.com>
2023-01-05 20:14:33 -0300

    change js

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-const correctAnswers = [
-  'question1d',
-  'question2d',
-  'question3b',
-  'question4d',
-  'question5e',
-  'question6c',
-  'question7c',
-  'question8a',
-  'question9c',
-  'question10d',
-];
-
-function checkAnswers(event) {
-  event.preventDefault();
-
-  let i;
-
-  for (i = 0; i < 10; i++) {
-    const isCorrect = document.getElementById(correctAnswers[i]).checked;
-    const currentParagraph = document.getElementsByClassName('result')[i];
-
-    if (isCorrect) {
-      currentParagraph.textContent = 'Correct!';
-    } else {
-      currentParagraph.textContent = 'Wrong!';
-    }
-  }
-}
+const correctAnswers = ['D', 'D', 'B', 'D', 'E', 'C', 'C', 'A', 'C', 'D'];
 
 const form = document.querySelector('form');
+const finalResult = document.querySelector('.result');
 
-form.addEventListener('submit', checkAnswers);
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  let score = 0;
+
+  const userAnswers = [
+    form.question1.value,
+    form.question2.value,
+    form.question3.value,
+    form.question4.value,
+    form.question5.value,
+    form.question6.value,
+    form.question7.value,
+    form.question8.value,
+    form.question9.value,
+    form.question10.value,
+  ];
+  userAnswers.forEach((userAnswer, index) => {
+    if (userAnswer === correctAnswers[index]) {
+      score += 10;
+    }
+  });
+
+  scrollTo(0, 0);
+
+  finalResult.querySelector('span').textContent = `${score}%`;
+  finalResult.classList.remove('d-none');
+
+  let counter = 0;
+
+  const timer = setInterval(() => {
+    if (counter === score) {
+      clearInterval(timer);
+    }
+
+    finalResult.querySelector('span').textContent = `${counter}%`;
+    counter++;
+  }, 10);
+});
